0c49867356fb414ec3e7e78356b5a06ffb5bab04
Kiryl Harbacheuski <kiryl.harbacheuski@soft-fx.lv>
2024-07-01 13:01:37 +0300

    aliases

--- src/components/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
-}
-
-export default Page
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	
 	return {
-		plugins: [react()],
+		plugins: [react(), tsconfigPaths()],
 		build: {
 			outDir: 'public',
-		},
-		resolve: {
-			alias: [
-				{
-					find: /^~(.+)/,
-					replacement: path.join(process.cwd(), 'node_modules/$1'),
-				},
-				{
-					find: /^src(.+)/,
-					replacement: path.join(process.cwd(), 'src/$1'),
-				},
-			],
 		},
 		define: {
             'process.env.REACT_APP_BASE_API_URL': JSON.stringify(env.REACT_APP_BASE_API_URL),
         },
 		server: {
-			port: 3000,
-		},
-		preview: {
 			port: 3000,
 		},
 	}
